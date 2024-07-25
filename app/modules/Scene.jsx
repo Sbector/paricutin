7028886797b037c036a9ac031be11c110b1c61fd
Sbector <saulbector@gmail.com>
2024-07-24 21:08:35 -0600

    Update model

--- app/modules/Scene.jsx
+++ app/modules/Scene.jsx
@@ -7,15 +7,15 @@ import Model from "./Model";
 export default function Scene() {
   return (
     <Canvas
-      camera={{ position: [0, 0, 1.5] }}
+      camera={{ position: [0, 0, 40] }}
       gl={{ toneMapping: ACESFilmicToneMapping, toneMappingExposure: 2.5 }}
     >
       <ambientLight intensity={3} />
       <Model />
       <TrackballControls
         target={[0, 0, 0]}
-        minDistance={0.9}
-        maxDistance={3}
+        minDistance={30}
+        maxDistance={90}
         noPan
         rotateSpeed={4}
       />
