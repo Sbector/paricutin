bf3b335fb6d8dbcbd789e46049cd7ee5faff7493
Sbector <saulbector@gmail.com>
2025-02-26 21:16:28 -0600

    Add placeholder

--- app/modules/Scene.jsx
+++ app/modules/Scene.jsx
@@ -4,6 +4,7 @@ import { Environment, TrackballControls } from "@react-three/drei";
 import { ACESFilmicToneMapping } from "three";
 import Paricutin from "./Paricutin";
 import { Suspense } from "react";
+import Placeholder from "./PlaceHolder";
 
 export default function Scene() {
   return (
@@ -12,20 +13,20 @@ export default function Scene() {
       gl={{ toneMapping: ACESFilmicToneMapping, toneMappingExposure: 1 }}
     >
       <ambientLight intensity={2} />
-      <directionalLight intensity={0.1} position={[2,2,1]} color={'#ffffff'}/>
-      <directionalLight intensity={0.2} position={[-4,0,-1]} color={'#ffffff'}/>
+      <directionalLight intensity={0.1} position={[2, 2, 1]} color={'#ffffff'} />
+      <directionalLight intensity={0.2} position={[-4, 0, -1]} color={'#ffffff'} />
 
       <Environment preset="warehouse"/>
-      
+
       <TrackballControls
         target={[0, 0, 0]}
         minDistance={1}
         maxDistance={3}
         noPan
-        rotateSpeed={4}
+        rotateSpeed={2.5}
       />
-      <Suspense>
-        <Paricutin rotation={[2, 1, 0]}/>
+      <Suspense fallback={<Placeholder />}>
+        <Paricutin />
       </Suspense>
     </Canvas>
   );
